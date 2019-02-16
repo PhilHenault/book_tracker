5199f4338018e20fa0d3194e399c448093b7e724
Phil Henault <philhen27@gmail.com>
2019-02-15 23:39:10 -0500

    added GET book request and error handling

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, make_response
 import config
 from pymongo import MongoClient
 from bson.json_util import loads
@@ -16,20 +16,40 @@ def index():
 	return "Welcome to API!"
 
 
-#takes in a list and restructures data by removing the "_id" key to make the response more readable
-def restructure_data(data_list):
-	to_return = []
-	#iterate through 
-	for item in data_list:
-		#delete "_id" for each
-		del item["_id"]
-		#re add the element in order to return it 
-		to_return.append(item)
-	return to_return
+#takes in a list or single element and restructures data by removing the "_id" key to make the response more readable
+def restructure_data(data, is_list = True):
+	
+	#handle if list
+	if is_list:
+		to_return = []
+		#iterate through 
+		for item in data:
+			#delete "_id" for each
+			del item["_id"]
+			#re add the element in order to return it 
+			to_return.append(item)
+		return to_return
+
+	#if not a list
+	else:
+		del data["_id"]
+		return data
+
+	
+#custom error handler to create own error response 
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'Error': 'Not Found'}), 404)
+
+
+#custom error handler to create own error response 
+@app.errorhandler(400)
+def not_found(error):
+    return make_response(jsonify({'Error': 'Invalid Request'}), 400)
 
 
 #route to handle get requests for getting all books
-@app.route('/book-keeper/api/books', methods = ['GET'])
+@app.route('/book_keeper/api/books', methods = ['GET'])
 def get_books():
 	#query the collection and restructure books
 	books_response = restructure_data(list(books.find({})))
@@ -37,7 +57,29 @@ def get_books():
 	return jsonify({"books": books_response})
 
 
+#route to handle getting single book based on book id
+@app.route('/book_keeper/api/book/<book_id>', methods = ['GET'])
+def get_book(book_id):
 
+	#if book id is not a numeric value return error
+	try:
+		book_id = int(book_id)
+	except:
+		abort(400)
+
+
+	#if book id is numeric, do lookup for it
+	book = books.find_one({"book_id" : int(book_id)})
+	
+	#no books found, return resource not found error 
+	if book is None:
+		abort(404)
+
+	#restructure data to remove "_id"
+	book_response = restructure_data(book, False)
+	#convert to json response format and return 
+	return jsonify({"book": book_response})
+		
 
 if __name__ == '__main__':
     app.secret_key = config.secret_key
